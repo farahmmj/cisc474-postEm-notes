e48d6d3122727be8354ad4f603e6cfe12830ae1d
Ntsee <ntseendin@gmail.com>
2020-12-05 00:26:39 -0500

    Added signup page to angular project

--- FrontEnd/src/app/signup/signup.component.ts
+++ FrontEnd/src/app/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
@@ -7,6 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
+  signupForm = new FormGroup({
+    email: new FormControl(''),
+    username: new FormControl(''),
+    password: new FormControl(''),
+    confirmPassword: new FormControl(''),
+  });
+  
   constructor() { }
 
   ngOnInit(): void {
