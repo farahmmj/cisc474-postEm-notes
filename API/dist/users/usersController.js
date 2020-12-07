7e7a52c4040168ed8abc061567ed0d34ceb58c03
jcwise98 <jcwise@udel.edu>
2020-12-07 00:11:15 -0500

    Get notes

--- API/dist/users/usersController.js
+++ API/dist/users/usersController.js
@@ -71,8 +71,8 @@ var UsersController = /** @class */ (function () {
         var id = MongoDB_1.Database.stringToId(req.params.id);
         var username = req.params.username;
         //const notes_id = req.params.notes_id;
-        UsersController.db.getOneRecord(UsersController.usersTable, { username: username })
-            .then(function (results) { return res.send({ fn: 'getUser', status: 'success', data: results.notes[0] }).end(); })
+        UsersController.db.getRecords(UsersController.usersTable)
+            .then(function (results) { return res.send({ fn: 'getUser', status: 'success', data: results }).end(); })
             .catch(function (reason) { return res.status(500).send(reason).end(); });
     };
     UsersController.prototype.updateUser = function (req, res) {
--- API/src/users/usersController.ts
+++ API/src/users/usersController.ts
@@ -94,8 +94,8 @@ export class UsersController {
          const id = Database.stringToId(req.params.id);
          const username = req.params.username;
          //const notes_id = req.params.notes_id;
-         UsersController.db.getOneRecord(UsersController.usersTable, {username: username})
-             .then((results) => res.send({ fn: 'getUser', status: 'success', data: results.notes[0] }).end())
+         UsersController.db.getRecords(UsersController.usersTable)
+             .then((results) => res.send({ fn: 'getUser', status: 'success', data: results }).end())
              .catch((reason) => res.status(500).send(reason).end());
  
      }
