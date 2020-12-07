5e12447e01a7cbb2e8d6873cc639ebdf2a88fba3
Ntsee <ntseendin@gmail.com>
2020-12-06 20:54:33 -0500

    Added file upload component form validation

--- FrontEnd/src/app/upload-file/upload-file.component.ts
+++ FrontEnd/src/app/upload-file/upload-file.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { PostEmService } from '../post-em.service';
 
 @Component({
   selector: 'app-upload-file',
@@ -7,9 +10,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UploadFileComponent implements OnInit {
 
-  constructor() { }
+  loading = false;
+  uploadForm = new FormGroup({
+    class: new FormControl('', [Validators.required]),
+    professor: new FormControl('', [Validators.required]),
+    file: new FormControl('', [Validators.required]),
+  });
+
+  constructor(public svc:PostEmService, public router:Router) {}
+
+  get class() {
+    return this.uploadForm.get('class');
+  }
+
+  get professor() {
+    return this.uploadForm.get('professor');
+  }
+
+  get file() {
+    return this.uploadForm.get('file');
+  }
 
   ngOnInit(): void {
+  }
+
+  upload() {
+    // to do: not implemented
   }
 
 }
